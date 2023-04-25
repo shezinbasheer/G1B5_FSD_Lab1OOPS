a437d53f7687750ecadfdfd816c1015162f6dc12
shezinbasheer <116833920+shezinbasheer@users.noreply.github.com>
2023-04-25 20:11:22 +0300

    Add files via upload

---
+++ Shezin_Bracket_Search/DriverApp.java
@@ -0,0 +1,78 @@
+package com.greatlearning.iitr.dsa.labsession3_1;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DriverApp {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the expression");
+
+		String input = sc.next();
+
+		if (checkExpression(input))
+
+			System.out.println("Valid expression");
+
+		else
+
+			System.out.println("Invalid expression");
+
+	}
+
+	private static boolean checkExpression(String input) {
+
+		Stack<Character> stack = new Stack<>();
+
+		for (char k : input.toCharArray())
+
+		{
+			if (k == '{' || k == '[' || k == '(')
+
+			{
+				stack.push(k);
+				continue;
+
+			}
+
+			if (stack.isEmpty())
+
+				return false;
+
+			char z;
+
+			switch (k)
+
+			{
+			case '}':
+
+				z = stack.pop();
+				if (z == '[' || z == '(')
+					return false;
+				break;
+
+			case ']':
+
+				z = stack.pop();
+				if (z == '{' || z == '(')
+					return false;
+				break;
+
+			case ')':
+
+				z = stack.pop();
+				if (z == '{' || z == '[')
+					return false;
+				break;
+			}
+
+		}
+
+		return (stack.isEmpty());
+
+	}
+
+}
